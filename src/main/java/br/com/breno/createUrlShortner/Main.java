f4cde253480cbb985426f85707de4558b4a7a1df
brenofelips <brendo.filipe2050@outlook.com>
2024-11-23 23:00:37 -0300

    create connection with s3 and save data in bucket

--- src/main/java/br/com/breno/createUrlShortner/Main.java
+++ src/main/java/br/com/breno/createUrlShortner/Main.java
@@ -3,6 +3,9 @@ package br.com.breno.createUrlShortner;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +13,7 @@ import java.util.UUID;
 
 public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final S3Client s3Client = S3Client.builder().build();
 
     @Override
     public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
@@ -19,17 +23,30 @@ public class Main implements RequestHandler<Map<String, Object>, Map<String, Str
         try {
             bodyMap = objectMapper.readValue(body, Map.class);
         } catch (Exception e) {
-            throw new RuntimeException("Error parsing JSON body" + e.getMessage() + e);
+            throw new RuntimeException("Error parsing JSON body" + e.getMessage(), e);
         }
 
         String originalUrl = bodyMap.get("originalUrl");
         String expirationTime = bodyMap.get("expirationTime");
         String shorUrlCode = UUID.randomUUID().toString().substring(0, 8);
+        long expirationTimeInSecond = Long.parseLong(expirationTime) * 3600;
+
+        UrlData urlData = new UrlData(originalUrl, expirationTimeInSecond);
+
+        try {
+            String urlObjectJson = objectMapper.writeValueAsString(urlData);
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket("aws-bucket-url-shortener")
+                    .key(shorUrlCode + "json") // nome do arquivo
+                    .build();
+
+            s3Client.putObject(request, RequestBody.fromString(urlObjectJson));
+        } catch (Exception e) {
+            throw new RuntimeException("Error saving data to S3: " + e.getMessage(), e);
+        }
 
         Map<String, String> response = new HashMap<>();
         response.put("codeId", shorUrlCode);
-        response.put("originalUrl", originalUrl);
-        response.put(" ", expirationTime);
         return response;
-    }
+    } 
 }
